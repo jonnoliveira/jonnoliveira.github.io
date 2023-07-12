db35bafb7009171adbe12c78cdd1a2df66dc7d93
jonnoliveira <jonnutri@gmail.com>
2023-07-12 12:15:29 -0300

    Delete: old portfolio files

--- src/data/Breakpoints.js
+++
@@ -1,8 +0,0 @@
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 },
-];
-
-export default breakPoints;
