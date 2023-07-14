1acae58e2a776a851aed7f80e986744ef1b713e8
jonnoliveira <jonnutri@gmail.com>
2023-07-13 21:27:45 -0300

    Add: little adjustments

--- src/components/TimeData.jsx
+++ src/components/TimeData.jsx
@@ -3,31 +3,36 @@ import React from 'react'
 function TimeData({ year, title, duration, instituition, description }) {
   return (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
+
       <li className="mb-10 ml-4">
+
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
+
         <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-
-          <span className="inline-block px-2 py-1 font-semibold text-white bg-[#573e04] rounded-md">
+          <p className="inline-block px-2 py-1 font-semibold text-white bg-[#573e04] rounded-md">
             {year}
-          </span>
-          <span className="text-lg font-semibold text-[#573e04]">
+          </p>
+
+          <p className="text-lg font-semibold text-[#573e04]">
             {title}
-          </span>
-          <span className="my-1 text-sm font-nomral leadin-none text-stone-400">
+          </p>
+
+          <p className="my-1 text-sm font-nomral leadin-none text-stone-400">
             {duration} Year(s)
-          </span>
-          <span className="text-sm  font-semibold text-[#573e04]">
+          </p>
+
+          <p className="text-sm  font-semibold text-[#573e04]">
             {instituition}
-          </span>
-
+          </p>
         </p>
+
         <p>
-
-          <span className="my-2 text-base font-normal text-stone-500">{description}</span>
-
+          <p className="my-2 font-normal text-stone-500 text-justify">
+            {description}
+          </p>
         </p>
       </li>
-    </ol>
+    </ol >
   )
 }
 
