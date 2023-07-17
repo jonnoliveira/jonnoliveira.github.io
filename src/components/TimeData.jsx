878a70b098cba5733c47cf2b7b9c11562fbb5d8c
jonnoliveira <jonnutri@gmail.com>
2023-07-17 11:16:43 -0300

    fix: timeData text responsiveness

--- src/components/TimeData.jsx
+++ src/components/TimeData.jsx
@@ -9,21 +9,27 @@ function TimeData({ year, title, duration, instituition, description }) {
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
 
         <span className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
+
           <p className="inline-block px-2 py-1 font-semibold text-white bg-[#573e04] rounded-md dark:bg-white dark:text-black">
             {year}
           </p>
 
-          <p className="text-[14px] sm:text-lg font-semibold text-[#573e04] dark:text-white">
-            {title}
-          </p>
+          <div className="flex flex-col gap-1 sm:flex-row sm:max-w-full sm:gap-5">
 
-          <p className="my-1 text-[10px] sm:text-sm font-nomral leadin-none text-stone-400 ">
-            {duration} Year(s)
-          </p>
+            <p className="text-[14px] sm:text-lg font-semibold text-[#573e04] dark:text-white">
+              {title}
+            </p>
 
-          <p className="text-[10px] sm:text-sm font-semibold text-[#573e04] dark:text-white">
-            {instituition}
-          </p>
+            <p className="my-1 text-[12px] sm:text-sm font-nomral leadin-none text-stone-400 ">
+              {duration} Year(s)
+            </p>
+
+            <p className="text-[12px] sm:text-sm font-semibold text-[#573e04] dark:text-white">
+              {instituition}
+            </p>
+
+          </div>
+
         </span>
 
         <span>
